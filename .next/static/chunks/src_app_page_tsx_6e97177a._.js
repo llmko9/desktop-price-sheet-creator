fbb96d1f0acf7670f917ac7e787324aced4b0bef
llmko9 <gregorimontilla09@gmail.com>
2025-07-23 17:33:59 -0400

    Add .next/static/chunks/src_app_page_tsx_6e97177a._.js 1753306438597

---
+++ .next/static/chunks/src_app_page_tsx_6e97177a._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_3fbd1c25._.js",
+  "static/chunks/node_modules_motion-dom_dist_es_27804559._.js",
+  "static/chunks/node_modules_framer-motion_dist_es_906fdbc6._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_jsbarcode_bin_e5c2e72f._.js",
+  "static/chunks/node_modules_html2canvas_dist_html2canvas_3938cdad.js",
+  "static/chunks/node_modules_20fdec58._.js"
+],
+    source: "dynamic"
+});
